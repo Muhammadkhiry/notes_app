c90d71d58eacfdf56787990fe7a3da51bfe4aef6
Muhammad-Khairy <mohamedkhiry121t@gmail.com>
2026-09-13 12:43:57 +0300

    add Hive packages, and initialize it

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,5 +1,17 @@
-class NoteModel {
-  final String title, subTitle, date;
+import 'package:hive_flutter/hive_flutter.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+
+  @HiveField(1)
+  final String subTitle;
+
+  @HiveField(2)
+  final String date;
+
+  @HiveField(3)
   final int color;
 
   NoteModel({
